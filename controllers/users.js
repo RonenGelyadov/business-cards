341a1a009449f2d58a3db1ff489220c868503cba
RonenGelyadov <ronengtv@gmail.com>
2026-08-25 01:04:11 +0300

    added register and login

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,108 @@
 import { Router } from 'express';
+import { model, Schema } from 'mongoose';
+import config from 'config';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = config.get('JWT_SECRET');
+
+const NameSchema = new Schema({
+  first: String,
+  middle: String,
+  last: String,
+});
+
+const ImageSchema = new Schema({
+  url: String,
+  alt: String,
+});
+
+const AddressSchema = new Schema({
+  state: { type: String, default: 'not defined' },
+  country: String,
+  city: String,
+  street: String,
+  houseNumber: String,
+  zip: { type: Number, default: 0 },
+});
+
+const UserSchema = new Schema({
+  name: NameSchema,
+  phone: String,
+  email: String,
+  password: String,
+  image: ImageSchema,
+  address: AddressSchema,
+  isAdmin: { type: Boolean, default: false },
+  isBusiness: Boolean,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = model('users', UserSchema);
 
 const router = Router();
+
+router.get('/', async (req, res) => {});
+
+router.post('/', async (req, res) => {
+  const { name, phone, email, password, image, address, isBusiness } = req.body;
+
+  const foundUser = await User.findOne({ email });
+
+  if (foundUser) {
+    return res.status(409).send({ message: 'email already exists' });
+  }
+
+  const user = new User({
+    name: {
+      first: name.first,
+      middle: name.middle,
+      last: name.last,
+    },
+    phone,
+    email,
+    password: await bcrypt.hash(password, 10),
+    image: {
+      url: image.url,
+      alt: image.alt,
+    },
+    address: {
+      state: address.state,
+      country: address.country,
+      city: address.city,
+      street: address.street,
+      houseNumber: address.houseNumber,
+    },
+    isBusiness,
+  });
+
+  const newUser = await user.save();
+  res.status(201).send(newUser);
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const foundUser = await User.findOne({ email });
+
+  if (!foundUser) {
+    return res.status(401).send({ message: 'Invalid credentials' });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, foundUser.password);
+
+  if (!passwordMatch) {
+    return res.status(401).send({ message: 'Invalid credentials' });
+  }
+
+  const tokenData = {
+    userId: foundUser._id,
+    fullName: `${foundUser.name.first} ${foundUser.name.last}`,
+    isAdmin: foundUser.isAdmin,
+  };
+
+  const token = jwt.sign(tokenData, JWT_SECRET, { expiresIn: '20m' });
+  res.status(200).send(token);
+});
 
 export default router;
--- server.js
+++ server.js
@@ -1,3 +1,36 @@
 import express from 'express';
+import config from 'config';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import UsersRouter from './controllers/users.js';
+import CardsRouter from './controllers/cards.js';
+
+const PORT = config.get('PORT');
+const DB_URL = config.get('DATABASE.URL');
+
+await mongoose.connect(DB_URL);
+console.log('mongodb connected');
 
 const server = express();
+
+server.use(express.json());
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  }),
+);
+
+server.use('/users', UsersRouter);
+server.use('/cards', CardsRouter);
+
+server.get('/', (req, res) => {
+  res.send({ message: 'Welcome to Business Cards' });
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
